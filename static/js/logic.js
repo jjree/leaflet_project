8fbab2d2050dd48501a8c2cb388d8d1165257215
Jason J. Ree <jasonjree@gmail.com>
2021-09-30 14:01:49 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -128,13 +128,17 @@ function createMap(earthquakes, plates) {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox/dark-v10',
+        tileSize: 512,
+        zoomOffset: -1,
         accessToken: API_KEY
     });
   
     var satellitemap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
-        id: 'mapbox/satellite-v9',
+        id: 'mapbox/satellite-v10',
+        tileSize: 512,
+        zoomOffset: -1,
         accessToken: API_KEY
     });
 
